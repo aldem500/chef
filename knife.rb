4a264356423ab64b6a45dfbf50bdf33969ec5b75
Alex Demenschonok <aldem@500px.com>
2014-01-01 18:57:13 -0500

    added files and cookdirectory

---
+++ knife.rb
@@ -0,0 +1,14 @@
+#
+current_dir = File.dirname(__FILE__)
+log_level		:info
+log_location		STDOUT
+node_name               "mychef" 
+client_key               "/etc/chef-server/admin.pem"
+validation_client_name   "chef-validator"
+validation_key           "/etc/chef-server/chef-validator.pem"
+chef_server_url          "https://mychef"
+cookbook_path            ["/work/chef/cookbooks"]
+cookbook_copyright 	"Aldemware"
+cookbook_license 	"apachev2"
+cookbook_email 		"cookbooks@aldemware.com"
+
