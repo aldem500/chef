075527c0c847f2eff8cde7e9f192864138682eaf
Alex Demenschonok <aldem@500px.com>
2014-10-14 16:28:30 -0400

    fixing

--- cookbooks/mysql/attributes/default.rb
+++ cookbooks/mysql/attributes/default.rb
@@ -5,7 +5,7 @@ default[:mysql][:innodb_buffer_pool_size] = "1G"
 default[:mysql][:innodb_log_file_size] = "64M"
 default[:mysql][:bind] = '0.0.0.0'
 default[:mysql][:datadir] = "/var/lib/mysql"
-default[:mysql][:package_name] = "percona-server-server-5.6 percona-server-client-5.6"
+default[:mysql][:package_name] = "percona-server-server-5.6 percona-server-common-5.6 percona-server-client-5.6"
 default[:mysql][:innodb_thread_concurrency] = 2
 
 default[:mysql2][:bind] = '127.0.0.2'
