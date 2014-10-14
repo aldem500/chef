aa5ed649816c682dd4faa9def61ca11f81a5eda6
Alex Demenschonok <aldem@500px.com>
2014-10-14 16:02:13 -0400

    fixed installation dependancies

--- cookbooks/mysql/attributes/default.rb
+++ cookbooks/mysql/attributes/default.rb
@@ -4,8 +4,8 @@ default[:mysql][:server_id] = 1
 default[:mysql][:innodb_buffer_pool_size] = "1G"
 default[:mysql][:innodb_log_file_size] = "64M"
 default[:mysql][:bind] = '0.0.0.0'
-default[:mysql2][:datadir] = "/var/lib/mysql"
-default[:mysql][:package_name] = "percona-server-client-5.6"
+default[:mysql][:datadir] = "/var/lib/mysql"
+default[:mysql][:package_name] = "percona-server-server-5.6 percona-server-client-5.6"
 default[:mysql][:innodb_thread_concurrency] = 2
 
 default[:mysql2][:bind] = '127.0.0.2'
@@ -13,7 +13,6 @@ default[:mysql2][:datadir] = "/var/lib/mysql2"
 default[:mysql2][:innodb_buffer_pool_size] = "1G"
 default[:mysql2][:innodb_log_file_size] = "64M"
 default[:mysql2][:server_id] = 2
-default[:mysql][:package_name] = "percona-server-client-5.6"
 
 default[:mysql3][:innodb_log_file_size] = "64M"
 default[:mysql3][:datadir] = "/var/lib/mysql3"
